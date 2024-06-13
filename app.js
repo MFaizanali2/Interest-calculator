2d25a870a4eab3edf181f85020feeac8e597121f
Faizan Ali <154216258+MFaizanali2@users.noreply.github.com>
2024-06-14 01:49:37 +0500

    id change
    continue

--- app.js
+++ app.js
@@ -1,6 +1,11 @@
-let user_amount = document.getElementById("exampleInputEmail1");
-let user_rate = document.getElementById("exampleInputPassword1");
-let user_time = document.getElementById("exampleInputPassword2");
+let user_amount = document.getElementById("user1");
+let user_rate = document.getElementById("user2");
+let user_time = document.getElementById("user3");
 let principle = document.getElementById("principle");
 let interest = document.getElementById("Interest");
 let amount = document.getElementById("amount");
+
+function calculate(){
+    let sum = (user_amount.value * user_rate.value * user_time.value) / 100;
+    amount.innerText = user_amount.value + sum;
+}
