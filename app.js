e6b9e8bfd711b124051f0ce0c7636d47a235e8f5
Faizan Ali <154216258+MFaizanali2@users.noreply.github.com>
2024-06-14 01:19:18 +0500

    id get
    continue

---
+++ app.js
@@ -0,0 +1,6 @@
+let user_amount = document.getElementById("exampleInputEmail1");
+let user_rate = document.getElementById("exampleInputPassword1");
+let user_time = document.getElementById("exampleInputPassword2");
+let principle = document.getElementById("principle");
+let interest = document.getElementById("Interest");
+let amount = document.getElementById("amount");
